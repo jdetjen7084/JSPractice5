62b9671e9fb71787d875a96ca5058efba7fc167a
Justin Detjen <jdetjen@gmx.com>
2020-01-15 15:44:55 -0600

    Create user input validation

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,25 +1,44 @@
-const addMovieModal = document.getElementById('add-modal');
-const startAddMovieButton = document.querySelector('header button');
-const backdrop = document.getElementById('backdrop');
-const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const addMovieModal = document.getElementById("add-modal");
+const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
 
 const toggleBackdrop = () => {
-    backdrop.classList.toggle('visible');
+  backdrop.classList.toggle("visible");
 };
 
 const toggleMovieModal = () => {
-    addMovieModal.classList.toggle('visible');
-    toggleBackdrop();
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
-    toggleMovieModal();
-}
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+      alert('Please enter name, URL, and a rating between 1-5!');
+      return
+  }
+};
 
 const backdropClickHandler = () => {
-    toggleMovieModal();
-}
+  toggleMovieModal();
+};
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
-backdrop.addEventListener('click', toggleMovieModal);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie)
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
