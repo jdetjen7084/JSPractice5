cbbbfcc487161e8f36c1c94f17036ce80aadf39e
Justin Detjen <jdetjen@gmx.com>
2020-01-16 17:19:38 -0600

    Add confirmation logic for delete modal

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -16,7 +16,7 @@ const updateUI = () => {
     }
 };
 
-const deleteMovie = movieId => {
+const deleteMovieHandler = movieId => {
     let movieIndex = 0;
     for (const movie of movies) {
         if(movie.id === movieId) {
@@ -34,10 +34,13 @@ const closeMovieDeletionModal = () => {
     deleteMovieModal.classList.remove('visible');
 };
 
-const deleteMovieHandler = movieId => {
+const startDeleteMovieHandler = movieId => {
     deleteMovieModal.classList.add('visible');
     toggleBackdrop();
-    deleteMovie(movieId);
+    const cancelDeletionButton = deleteMovieModal.querySelector('.btn--passive');
+    const confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
+    cancelDeletionButton.addEventListener('click', closeMovieDeletionModal);
+    confirmDeletionButton.addEventListener('click', deleteMovieHandler.bind(null, movieId));
 };
 
 const renderNewMovieElement = (id, title, imageUrl, rating) => {
@@ -52,7 +55,7 @@ const renderNewMovieElement = (id, title, imageUrl, rating) => {
         <p>${rating}/5 stars</p>
     </div>
     `;
-    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id))
+    newMovieElement.addEventListener('click', startDeleteMovieHandler.bind(null, id));
     const listRoot = document.getElementById('movie-list');
     listRoot.append(newMovieElement);
 };
